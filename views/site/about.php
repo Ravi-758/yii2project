43f350282f8f4d5cc821c16201afecf9cdffbce8
ravi-758 <ravidhundhara2000.gmail>
2024-06-16 18:11:30 +0530

    git

--- views/site/about.php
+++ views/site/about.php
@@ -3,13 +3,26 @@
 /** @var yii\web\View $this */
 
 use yii\helpers\Html;
+use yii\bootstrap5\Breadcrumbs;
 
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
+
+$listArray =[
+    [
+        'label' =>  'Info',
+        'url' =>'info'
+    ],
+    [
+        'label' =>  'About',
+        'url' =>'about'
+    ]
+
+];
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
-
+    <?= Breadcrumbs::widget(['links' => $listArray]) ?>
     <p>
         This is the About page. You may modify the following file to customize its content:
     </p>
